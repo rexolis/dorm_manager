1d0dd110293c1e5c9dfb0c8fc4b7a97743f13222
Rex Olis <olis_1234@yahoo.com.ph>
2016-11-17 11:50:22 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   get 'users/new'
 
+  get 'users/new'
+
   root 'pages#home'
   
   get '/home' => 'pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102023445) do
+ActiveRecord::Schema.define(version: 20161117114108) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
