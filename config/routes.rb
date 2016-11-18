677d81bbed5c6c28e0bdb5e02e7473b0292d52ba
Rex Olis <olis_1234@yahoo.com.ph>
2016-11-18 00:51:47 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root 'pages#home'
   
   get '/home' => 'pages#home'
   get '/contact', to: 'pages#contact'
   get '/about', to: 'pages#about'
   get '/help', to: 'pages#help'
-  #get '/login', to: 'pages#login'
-  get '/signup', to: 'users#new'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
  
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117114108) do
+ActiveRecord::Schema.define(version: 20161117141048) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
